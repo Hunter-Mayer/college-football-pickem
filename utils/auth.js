daf1f48f35cdebb544d8138ee97f134a1e406242
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-12-05 23:02:11 -0500

    modified formatting

--- utils/auth.js
+++ utils/auth.js
@@ -1,9 +1,9 @@
 const withAuth = (req, res, next) => {
-  if (!req.session.logged_in) {
-    res.redirect('/login');
-  } else {
-    next();
-  }
+	if (!req.session.logged_in) {
+		res.redirect("/login");
+	} else {
+		next();
+	}
 };
 
 module.exports = withAuth;
