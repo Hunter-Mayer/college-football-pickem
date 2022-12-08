13fbca7d17e482d23d7d94acd4602fff466abbd7
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-12-08 00:36:55 -0500

    fixed merge conflicts in controllers/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 import express from "express";
 const router = express.Router();
 import apiRoutes from "./api";
-import homeRoutes from "./homeRoutes";
-=======
-const router = require("express").Router();
 
-const apiRoutes = require('./api');
-const viewRoutes = require('./viewRoutes');
->>>>>>> dev
-
-router.use('/', viewRoutes);
-router.use('/api', apiRoutes);
+router.use("/", viewRoutes);
+router.use("/api", apiRoutes);
 
 export default router;
