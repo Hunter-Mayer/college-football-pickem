0d544b870165512b0a0db3d54c319122bb144bb2
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-12-07 20:40:46 -0500

    implemented basic Game model

--- models/game.js
+++ models/game.js
@@ -1,8 +1,10 @@
 import { Model, DataTypes } from "sequelize";
 import sequelize from "../config/connection";
+import Team from "team";
 
 class Game extends Model {}
 
+// Each team has a home and away team rather than team_1 and team_2
 Game.init(
 	{
 		id: {
@@ -11,15 +13,27 @@ Game.init(
 			primaryKey: true,
 			autoIncrement: true,
 		},
-		team_1: {},
-		team_2: {},
+		home: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: Team,
+				key: "id",
+			},
+		},
+		away: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: Team,
+				key: "id",
+			},
+		},
 	},
 	{
 		sequelize,
-		timestamps: false,
+		timestamps: true,
 		freezeTableName: true,
 		underscored: true,
-		modelName: "user",
+		modelName: "game",
 	}
 );
 
