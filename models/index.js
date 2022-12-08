88bd251dcbeb153a817a0357553d9a78ee012aec
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-12-07 21:07:59 -0500

    implemented first round of associations

--- models/index.js
+++ models/index.js
@@ -2,6 +2,150 @@ import User from "user";
 import Pick from "pick";
 import Week from "week";
 import Game from "game";
+import Team from "team";
 import UserWeekPick from "userweekpick";
+
+// TODO: Implement associations
+// User associations
+User.hasMany(Pick, {
+	onDelete: "CASCADE",
+	foreignKey: "user_id",
+});
+
+// Pick associations
+Pick.hasOne(Game, {
+	foreignKey: "game_id",
+});
+
+Pick.hasOne(Team, {
+    foreignKey: "pick",
+})
+
+Pick.belongsTo(User, {
+	foreignKey: "user_id",
+});
+
+// Week associations
+
+Week.hasOne(Game, {
+    foreignKey: "id",
+    as: "game_1",
+});
+
+Week.hasOne(Game, {
+    foreignKey: "id",
+    as: "game_2",
+});
+
+Week.hasOne(Game, {
+    foreignKey: "id",
+    as: "game_3",
+});
+
+Week.hasOne(Game, {
+    foreignKey: "id",
+    as: "game_4",
+});
+
+Week.hasOne(Game, {
+    foreignKey: "id",
+    as: "game_5",
+});
+
+Week.hasOne(Game, {
+    foreignKey: "id",
+    as: "game_6",
+});
+
+Week.hasOne(Game, {
+    foreignKey: "id",
+    as: "game_7",
+});
+
+Week.hasOne(Game, {
+    foreignKey: "id",
+    as: "game_8",
+});
+
+Week.hasOne(Game, {
+    foreignKey: "id",
+    as: "game_9",
+});
+
+Week.hasOne(Game, {
+    foreignKey: "id",
+    as: "game_10",
+});
+
+// Game associations
+Game.belongsToMany(Pick {
+    foreignKey: "game_id"
+});
+
+Game.hasOne(Team, {
+    foreignKey: "id",
+    as: "home",
+});
+
+Game.hasOne(Team, {
+    foreignKey: "id",
+    as: "away",
+});
+
+Game.belongsTo(Week, {
+    foreignKey: "game_1",
+});
+
+Game.belongsTo(Week, {
+    foreignKey: "game_2",
+});
+
+Game.belongsTo(Week, {
+    foreignKey: "game_3",
+});
+
+Game.belongsTo(Week, {
+    foreignKey: "game_4",
+});
+
+Game.belongsTo(Week, {
+    foreignKey: "game_5",
+});
+
+Game.belongsTo(Week, {
+    foreignKey: "game_6",
+});
+
+Game.belongsTo(Week, {
+    foreignKey: "game_7",
+});
+
+Game.belongsTo(Week, {
+    foreignKey: "game_8",
+});
+
+Game.belongsTo(Week, {
+    foreignKey: "game_9",
+});
+
+Game.belongsTo(Week, {
+    foreignKey: "game_10",
+});
+
+// Team associations
+Team.belongsToMany(Game, {
+    foreignKey: "home",
+});
+
+Team.belongsToMany(Game, {
+    foreignKey: "away",
+})
+
+Team.belongsToMany(Pick, {
+    foreignKey: "pick",
+})
+
+// UserWeekPick associations
+// TODO: Maybe this isn't needed...
 
 export { User, Pick, Week, Game, UserWeekPick };
