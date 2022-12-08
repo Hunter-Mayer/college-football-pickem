4084f6e239bf9e3ae644de247217affdbe2a0753
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-12-07 20:34:21 -0500

    implemented basic team model

---
+++ models/team.js
@@ -0,0 +1,36 @@
+import { Model, DataTypes } from "sequelize";
+import sequelize from "../config/connection";
+
+class Team extends Model {}
+
+Team.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		// Link to Sports Team Logo
+		logo: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				isURL: true,
+			},
+		},
+	},
+	{
+		sequelize,
+		timestamps: true,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "team",
+	}
+);
+
+export default Team;
