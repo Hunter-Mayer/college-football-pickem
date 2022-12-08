f37ccf3d7faecd1411e4524680e25e83e3904d08
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-12-07 20:42:32 -0500

    implemented basic junction model for userweekpick

--- models/userweekpick.js
+++ models/userweekpick.js
@@ -1,5 +1,8 @@
 import { Model, DataTypes } from "sequelize";
 import sequelize from "../config/connection";
+import User from "user";
+import Week from "week";
+import Pick from "pick";
 
 class UserWeekPick extends Model {}
 
@@ -11,13 +14,34 @@ UserWeekPick.init(
 			primaryKey: true,
 			autoIncrement: true,
 		},
+		user_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: User,
+				key: "id",
+			},
+		},
+		week_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: Week,
+				key: "id",
+			},
+		},
+		pick_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: Pick,
+				key: "id",
+			},
+		},
 	},
 	{
 		sequelize,
-		timestamps: false,
+		timestamps: true,
 		freezeTableName: true,
 		underscored: true,
-		modelName: "user",
+		modelName: "userweekpick",
 	}
 );
 
