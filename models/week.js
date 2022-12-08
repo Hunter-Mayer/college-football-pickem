f1587a0ccfa6e9bfc8bcd9edee3e6b10733f9286
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-12-07 23:16:40 -0500

    formatting in week.js

--- models/week.js
+++ models/week.js
@@ -4,9 +4,6 @@ import Game from "./game";
 
 class Week extends Model {}
 
-// TODO: Generate algorithm to dynamically assign a list of Game IDs instead of hardcoded columns
-// TODO: Create a table dynamically for each week and use that table to store the list of games?
-// TODO: Unsure how hardcoding games 1 through 10 will work with the controllers...
 Week.init(
 	{
 		id: {
@@ -15,84 +12,15 @@ Week.init(
 			primaryKey: true,
 			autoIncrement: true,
 		},
-		game_1: {
+		week_num: {
 			type: DataTypes.INTEGER,
 			allowNull: false,
-			references: {
-				model: Game,
-				key: "id",
-			},
 		},
-		game_2: {
+		game_id: {
 			type: DataTypes.INTEGER,
-			allowNull: false,
 			references: {
 				model: Game,
-				key: "id",
-			},
-		},
-		game_3: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			references: {
-				model: Game,
-				key: "id",
-			},
-		},
-		game_4: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			references: {
-				model: Game,
-				key: "id",
-			},
-		},
-		game_5: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			references: {
-				model: Game,
-				key: "id",
-			},
-		},
-		game_6: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			references: {
-				model: Game,
-				key: "id",
-			},
-		},
-		game_7: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			references: {
-				model: Game,
-				key: "id",
-			},
-		},
-		game_8: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			references: {
-				model: Game,
-				key: "id",
-			},
-		},
-		game_9: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			references: {
-				model: Game,
-				key: "id",
-			},
-		},
-		game_10: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			references: {
-				model: Game,
-				key: "id",
+				foreignKey: "id",
 			},
 		},
 	},
