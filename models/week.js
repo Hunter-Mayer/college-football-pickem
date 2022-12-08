513d4d9696236efd5a4b928933e91b8368757ca7
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-12-07 20:10:40 -0500

    Added model attributes to week.js. Basic model complete

--- models/week.js
+++ models/week.js
@@ -3,6 +3,9 @@ import sequelize from "../config/connection";
 
 class Week extends Model {}
 
+// TODO: Generate algorithm to dynamically assign a list of Game IDs instead of hardcoded columns
+// TODO: Create a table dynamically for each week and use that table to store the list of games?
+// TODO: Unsure how hardcoding games 1 through 10 will work with the controllers...
 Week.init(
 	{
 		id: {
@@ -11,13 +14,93 @@ Week.init(
 			primaryKey: true,
 			autoIncrement: true,
 		},
+		game_1: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: {
+				model: Game,
+				key: "id",
+			},
+		},
+		game_2: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: {
+				model: Game,
+				key: "id",
+			},
+		},
+		game_3: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: {
+				model: Game,
+				key: "id",
+			},
+		},
+		game_4: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: {
+				model: Game,
+				key: "id",
+			},
+		},
+		game_5: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: {
+				model: Game,
+				key: "id",
+			},
+		},
+		game_6: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: {
+				model: Game,
+				key: "id",
+			},
+		},
+		game_7: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: {
+				model: Game,
+				key: "id",
+			},
+		},
+		game_8: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: {
+				model: Game,
+				key: "id",
+			},
+		},
+		game_9: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: {
+				model: Game,
+				key: "id",
+			},
+		},
+		game_10: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: {
+				model: Game,
+				key: "id",
+			},
+		},
 	},
 	{
 		sequelize,
-		timestamps: false,
+		timestamps: true,
 		freezeTableName: true,
 		underscored: true,
-		modelName: "user",
+		modelName: "week",
 	}
 );
 
