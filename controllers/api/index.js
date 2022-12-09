4cc7568bf43dfdcbcfb6254896968365e80aea7e
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-12-08 22:04:08 -0500

    added api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,15 @@
 import express from "express";
 const router = express.Router();
+import gameRoutes from "./gameRoutes";
+import pickRoutes from "./pickRoutes";
+import teamRoutes from "./teamRoutes";
 import userRoutes from "./userRoutes";
+import weekRoutes from "./weekRoutes";
 
+router.use("/game", gameRoutes);
+router.use("/pick", pickRoutes);
+router.use("/team", teamRoutes);
 router.use("/users", userRoutes);
+router.use("/week", weekRoutes);
 
 export default router;
