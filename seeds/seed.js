992303fcef9420a578762b70bead8b77aa206533
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-12-08 19:56:37 -0500

    added bulk create methods to each model

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,11 +1,41 @@
 import sequelize from "../config/connection";
-import { User } from "../models";
+import { Date, Game, Pick, Team, User, Week } from "../models";
+import dateData from "./dateData.json" assert { type: "json" };
+import gameData from "./gameData.json" assert { type: "json" };
+import pickData from "./pickData.json" assert { type: "json" };
+import teamData from "./teamData.json" assert { type: "json" };
 import userData from "./userData.json" assert { type: "json" };
+import weekData from "./weekData.json" assert { type: "json" };
 
 const seedDatabase = async () => {
 	await sequelize.sync({ force: true });
 
+	await Date.bulkCreate(dateData, {
+		individualHooks: true,
+		returning: true,
+	});
+
+	await Game.bulkCreate(gameData, {
+		individualHooks: true,
+		returning: true,
+	});
+
+	await Pick.bulkCreate(pickData, {
+		individualHooks: true,
+		returning: true,
+	});
+
+	await Team.bulkCreate(teamData, {
+		individualHooks: true,
+		returning: true,
+	});
+
 	await User.bulkCreate(userData, {
+		individualHooks: true,
+		returning: true,
+	});
+
+	await Week.bulkCreate(weekData, {
 		individualHooks: true,
 		returning: true,
 	});
