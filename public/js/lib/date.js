3ba3da51348da1fdfc4acdfb26f61dda4c9827a2
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-12-11 23:01:14 -0500

    added create method within date class

--- public/js/lib/date.js
+++ public/js/lib/date.js
@@ -1,12 +1,27 @@
 export default class Date {
+	dateDatabaseURL = new URL("/api/dates");
+
 	constructor(year, month, day) {
 		this.year = year;
 		this.month = month;
 		this.day = day;
+
+		this.id = null;
 	}
 
-	create() {
+	async create() {
 		// TODO: Create date entry in the database
+		const res = await fetch(this.dateDatabaseURL, {
+			method: "POST",
+			body: JSON.stringify(this.getDate()),
+			headers: { "Content-Type": "application/json" },
+		});
+
+		if (res.ok) {
+			this.id = res.id;
+		} else {
+			alert("Failed to create date!");
+		}
 	}
 
 	getDate() {
