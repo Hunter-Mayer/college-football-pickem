342f04f26f01e618889caace93cf1b5c762d0f54
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-12-11 22:03:53 -0500

    added basic game post route

--- controllers/api/gameRoutes.js
+++ controllers/api/gameRoutes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { Sequelize } from "sequelize";
 const router = express.Router();
 import { Game, Team, Date, Week } from "../../models";
 
@@ -33,6 +34,30 @@ router.get("/", async (req, res) => {
 	} catch (err) {
 		console.error(err);
 		res.status(500).send(`<h1>500 Internal Server Error</h1>`);
+	}
+});
+
+router.post("/", async (req, res) => {
+	// Create a new game
+	/**
+	 * Request body JSON Format:
+	 * {
+	 * 	home_team_id: "home team id as an integer",
+	 * 	away_team_id: "away team id as an integer",
+	 * 	date_id: "date id as an integer"
+	 * }
+	 */
+	try {
+		const newGame = await Game.create(req.body);
+		res.status(201).json(newGame).send();
+	} catch (err) {
+		if (err instanceof Sequelize.ValidateError) {
+			res.status(400).send(`<h1>400 Bad Request!</h1>
+			<h3>Specified id does not exist.</h3>`);
+		} else {
+			res.status(500).send(`<h1>500 Internal Server Error</h1>`);
+		}
+		console.error(err);
 	}
 });
 
