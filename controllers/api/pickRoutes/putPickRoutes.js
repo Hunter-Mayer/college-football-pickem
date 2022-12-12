bd26d19bffb59563c3519489361ed8d757057040
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-12-11 21:44:43 -0500

    added put route for picks

--- controllers/api/pickRoutes/putPickRoutes.js
+++ controllers/api/pickRoutes/putPickRoutes.js
@@ -2,11 +2,57 @@ import express from "express";
 const router = express.Router();
 import { Game, Pick, Team, User, Date, Week } from "../../../models";
 
+// Uses query parameters...
 router.put("/", async (req, res) => {
-	try {
-	} catch (err) {
-		console.error(err);
-		res.status(500).send(`<h1>500 Internal Server Error</h1>`);
+	/**
+	 * req.query.id
+	 * req.query.team_pick_id
+	 * req.query.points
+	 */
+
+	const pickExists = async () => {
+		return (
+			await Pick.findAll({
+				attributes: ["id"],
+			})
+		)
+			.map((element) => element.dataValues.id)
+			.includes(Number(req.params.id));
+	};
+
+	if (pickExists) {
+		try {
+			const updatedPick = await Pick.update(
+				{
+					team_pick_id: req.query.team_pick_id,
+					points: req.query.points,
+				},
+				{ where: { id: req.query.id } }
+			);
+
+			// updatePick will have a length of 1 if the value was updated. Otherwise, no update was performed
+			if (updatedPick[0]) {
+				res.status(204).send();
+			} else {
+				res.status(304).send();
+			}
+		} catch (err) {
+			console.error(err);
+			res.status(500).send(`<h1>500 Internal Server Error</h1>`);
+		}
+	} else {
+		try {
+			const newPick = await Pick.create({
+				game_id: req.query.game_id,
+				team_pick_id: req.query.team_pick_id,
+				points: req.query.points,
+				user_id: req.query.user_id,
+			});
+			res.status(201).json(newPick).send();
+		} catch (err) {
+			console.error(err);
+			res.status(500).send(`<h1>500 Internal Server Error</h1>`);
+		}
 	}
 });
 
