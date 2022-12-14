a336e628eddab6a16a899745b29cb338cb46fb75
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-12-13 20:23:19 -0500

    added signup form validation

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -30,6 +30,7 @@ router.post("/login", async (req, res) => {
 
 			res.json({ user: userData, message: "You are now logged in!" });
 		});
+		res.redirect("/homepage");
 	} catch (err) {
 		res.status(400).json(err);
 	}
@@ -53,15 +54,11 @@ router.post("/signup", async (req, res) => {
 		if (userData) {
 			res.status(400).json({
 				message: "Email already exists",
-			})
+			});
 		}
-		const newUser = await User.create(
-			req.body
-		)
-			res.status(200).json(newUser)
-	} catch (error) {
-
-	}
-})
+		const newUser = await User.create(req.body);
+		res.status(200).json(newUser).send();
+	} catch (error) {}
+});
 
 export default router;
--- public/js/login.js
+++ public/js/login.js
@@ -22,35 +22,56 @@ document
 	.querySelector("#login-form")
 	.addEventListener("submit", loginFormHandler);
 
-
-
-
 const signupFormHandler = async (event) => {
 	event.preventDefault();
 
 	const name = document.querySelector("#name-signup").value.trim();
 	const email = document.querySelector("#email-signup").value.trim();
 	const password = document.querySelector("#password-signup").value.trim();
-	const confirmPassword = document.querySelector("#confirmPassword-signup").value.trim();
+	const confirmPassword = document
+		.querySelector("#confirmPassword-signup")
+		.value.trim();
 
-	console.log (name, email, password, confirmPassword)
-	if (email && password) {
+	// Form Entry Validation
+	let alertMessage = "";
+	if (!name) {
+		alertMessage += "Missing name\n";
+	}
+	if (!email) {
+		alertMessage += "Missing email\n";
+	}
+	if (!password) {
+		alertMessage += "Missing password\n";
+	}
+	if (password.length < 8) {
+		alertMessage += "Password is smaller than 8 characters\n";
+	}
+	if (!confirmPassword || password !== confirmPassword) {
+		alertMessage += "Passwords don't match\n";
+	}
+
+	if (alertMessage.length !== 0) {
+		alert(alertMessage);
+		return;
+	} else {
 		const response = await fetch("/api/users/signup", {
 			method: "POST",
 			body: JSON.stringify({ name, email, password }),
 			headers: { "Content-Type": "application/json" },
 		});
-		const userData = (await response.json())
-		console.log(userData)
-		if (response.ok) {
-			document.location.replace("/");
-		} else {
+		const userData = await response.json();
+		console.log(await userData);
+		if (!response.ok) {
 			alert("Failed to sign up");
 		}
+		// if (response.ok) {
+		// 	document.location.replace("/");
+		// } else {
+		// 	alert("Failed to sign up");
+		// }
 	}
 };
 
 document
 	.querySelector("#signup-form")
 	.addEventListener("submit", signupFormHandler);
-
