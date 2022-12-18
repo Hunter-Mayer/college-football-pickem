8dd0724515e7ba5a65bb8eb812c0356908f7b8c5
pbp66 <{ID}+{username}@users.noreply.github.com>
2022-12-18 15:15:40 -0500

    updated formating 2 spaces -> 4 spaces as tab

--- public/js/teampicker.js
+++ public/js/teampicker.js
@@ -1,12 +1,10 @@
-const editPicks = document.getElementById("editPick")
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-const radioButtons = document.querySelectorAll('input[name="customRadio"]')
-const pointSelections = document.querySelectorAll('option[selected]')
-const pickCards = document.querySelectorAll('div[class="card"]')
+const editPicks = document.getElementById("editPick");
+const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
+const radioButtons = document.querySelectorAll('input[name="customRadio"]');
+const pointSelections = document.querySelectorAll("option[selected]");
+const pickCards = document.querySelectorAll('div[class="card"]');
 
-const pickedTeams = document.querySelectorAll('input[checked]')
-
-
+const pickedTeams = document.querySelectorAll("input[checked]");
 
 // for (const radioButton of radioButtons) {
 //     radioButton.addEventListener("change", showSelected);
@@ -18,67 +16,62 @@ const pickedTeams = document.querySelectorAll('input[checked]')
 //     }
 // }
 
-
 // Prints all point selections
 // for (const pointSelection of pointSelections) {
 //     console.log(pointSelection.innerHTML)
 // }
 
 const alert = (message, type) => {
-  const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
+	const wrapper = document.createElement("div");
+	wrapper.innerHTML = [
+		`<div class="alert alert-${type} alert-dismissible" role="alert">`,
+		`   <div>${message}</div>`,
+		'   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+		"</div>",
+	].join("");
 
-  alertPlaceholder.append(wrapper)
-}
+	alertPlaceholder.append(wrapper);
+};
 
 if (editPicks) {
-  editPicks.addEventListener('click', () => {
-    alert('Your pick has been updated!', 'success')
-  })
+	editPicks.addEventListener("click", () => {
+		alert("Your pick has been updated!", "success");
+	});
 }
 
-
-
 editPicks.addEventListener("click", (event) => {
-    event.preventDefault();
-    
-    // Prints all pick ids
-    for (const pickCard of pickCards) {
-        console.log(pickCard.id)
-    }
-    console.log("-----------------------")
+	event.preventDefault();
 
-    // Prints picked_team_id
-    for (const radioButton of radioButtons) {
-        if (radioButton.checked) {
-            console.log(radioButton.id)
-        }
-    }
-    console.log("-----------------------")
-    for (const pointSelection of pointSelections) {
-        console.log(pointSelection.innerHTML)
-    }
-    console.log("-----------------------")
-})
+	// Prints all pick ids
+	for (const pickCard of pickCards) {
+		console.log(pickCard.id);
+	}
+	console.log("-----------------------");
 
+	// Prints picked_team_id
+	for (const radioButton of radioButtons) {
+		if (radioButton.checked) {
+			console.log(radioButton.id);
+		}
+	}
+	console.log("-----------------------");
+	for (const pointSelection of pointSelections) {
+		console.log(pointSelection.innerHTML);
+	}
+	console.log("-----------------------");
+});
 
-$(document).ready(function() {
-    $("select.selectVal").change(function() {
-        let selectedItem = $(this).children("option:selected").val();
-        console.log(selectedItem)
-      });
-  });
-
+$(document).ready(function () {
+	$("select.selectVal").change(function () {
+		let selectedItem = $(this).children("option:selected").val();
+		console.log(selectedItem);
+	});
+});
 
 // editPicks.addEventListener("click", (event) => {
 //     event.preventDefault();
 
-//     //const id = 
+//     //const id =
 
 //     if (event.submitter.innerHTML === "Submit") {
 //         fetch("/api/pick", {
@@ -90,7 +83,7 @@ $(document).ready(function() {
 //         })
 //         .then(response => {
 //             if (response.status === 200) {
-                
+
 //             }
 //         })
 //     }
