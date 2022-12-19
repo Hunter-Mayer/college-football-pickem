4da62fbce1be9a57488be6fbbf96d0e17248a147
pbp66 <{ID}+{username}@users.noreply.github.com>
2022-12-19 11:53:30 -0500

    picked team form now updates!

--- public/js/teampicker.js
+++ public/js/teampicker.js
@@ -18,7 +18,6 @@ function generateEmptyPicks(count) {
 }
 
 function updateFormSelectionData() {
-	let goodPicks = [];
 	let pickContainers = document.getElementsByClassName("pick-container");
 	let gameContainers = document.getElementsByClassName("game-container");
 	let pickedTeamForms = document.getElementsByClassName("team-picker-form");
@@ -32,15 +31,19 @@ function updateFormSelectionData() {
 		picks[i].id = pickContainers[i].id.replace("pick-", "");
 		picks[i].gameId = gameContainers[i].id.replace("game-", "");
 
-		for (const form of pickedTeamForms[i]) {
-			console.log(form);
-			// for (const inputElement of form) {
-			// 	if (inputElement.checked) {
-			// 		console.log(inputElement);
-			// 	}
-			// }
+		/**
+		 * If a team is selected, update its id in the pick entry. Otherwise, save the picked team as null
+		 */
+		let awayTeam = pickedTeamForms[i][0];
+		let homeTeam = pickedTeamForms[i][1];
+		if (awayTeam.checked) {
+			picks[i].pickedTeamId = awayTeam.id.replace("team-", "");
+		} else if (homeTeam.checked) {
+			picks[i].pickedTeamId = homeTeam.id.replace("team-", "");
+		} else {
+			picks[i].pickedTeamId = null;
 		}
-		//picks[i].pickedTeamId;
+
 		/**
 		 * To get the points, we must iterate over all available options and select the one that the user has selected via the HTML attribute selected
 		 */
@@ -53,16 +56,7 @@ function updateFormSelectionData() {
 
 	console.log(picks);
 
-	/**
-	 * If a partial list of picks were updated, warn the user that they submitted an incomplete form
-	 */
-	if (goodPicks.length === 0) {
-		return null;
-	} else if (goodPicks.length < pickContainers.length) {
-		return false;
-	} else {
-		return true;
-	}
+	return picks;
 }
 /**
  * Creates alert banner when you've submitted the picks. Styling provided by bootstrap
@@ -81,23 +75,10 @@ const alert = (message, type) => {
 
 editPicks.addEventListener("click", async (event) => {
 	event.preventDefault();
-	let updateStatus = updateFormSelectionData();
-	if (updateStatus === null) {
-		alert(
-			"You must select at least one team to update your picks",
-			"danger"
-		);
-		return;
-	} else if (!updateStatus) {
-		alert(
-			" Not all picks were updated. Resubmit when all picks are updated.",
-			"warning"
-		);
-	}
+	let picks = updateFormSelectionData();
 
 	const apiUrl = new URL(document.location.origin + "/api/pick");
-
-	for (let i = 0; i < pickIds.length; i++) {
+	for (let i = 0; i < picks.length; i++) {
 		/**
 		 * Need to provide
 		 * pick id
@@ -105,11 +86,13 @@ editPicks.addEventListener("click", async (event) => {
 		 * picked team id
 		 * points
 		 */
-		apiUrl.searchParams.set("id", pickIds[i]);
-		apiUrl.searchParams.set("game_id", gameIds[i]);
-		apiUrl.searchParams.set("team_pick_id", pickedTeamIds[i]);
-		apiUrl.searchParams.set("points", points[i]);
-		console.log(apiUrl);
+		apiUrl.searchParams.set("id", picks[i].id);
+		apiUrl.searchParams.set("game_id", picks[i].gameId);
+		apiUrl.searchParams.set("team_pick_id", picks[i].pickedTeamId);
+		apiUrl.searchParams.set("points", picks[i].points);
+		/**
+		 * Checks for valid pick objects and only makes a PUT request to the API of the pick is valid
+		 */
 		const response = await fetch(apiUrl, {
 			method: "PUT",
 			headers: {
@@ -123,6 +106,17 @@ editPicks.addEventListener("click", async (event) => {
 			console.error("Pick was not submitted");
 		}
 	}
-
+	// if (updateStatus === null) {
+	// 	alert(
+	// 		"You must select at least one team to update your picks",
+	// 		"danger"
+	// 	);
+	// 	return;
+	// } else if (!updateStatus) {
+	// 	alert(
+	// 		" Not all picks were updated. Resubmit when all picks are updated.",
+	// 		"warning"
+	// 	);
+	// }
 	alert("Your picks have been updated!", "success");
 });
